e50a290b4b9b4e329362e3f32f60f21bb7ce3648
Emilia <81705094+isemilia@users.noreply.github.com>
2024-08-09 16:17:08 +0500

    Initial commit

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <Table :rows="users" :headers="headers"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import Table from "./components/table/table.vue";
+
+  const users = [
+    {id: 1, name: 'Anna', email: 'anna@gmail.com' }
+  ]
+
+  const headers = [
+    {name: 'name', label: <h2>First name<h2/>}
+  ]
+</script>
+
+<style scoped lang="scss">
+  .main {
+    height: 100%;
+    width: 100%;
+    max-width: 700px;
+    margin: 0 auto;
+  }
+</style>
